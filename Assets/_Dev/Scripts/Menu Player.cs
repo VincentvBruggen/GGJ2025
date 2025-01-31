368fab0f5464466a9015a10ab694ed9634933cd7
Linkerpink <noahvanuunen2007@gmail.com>
2025-01-31 19:45:23 +0100

    ja 2

--- Assets/_Dev/Scripts/Menu Player.cs
+++ Assets/_Dev/Scripts/Menu Player.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MenuPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerInput m_playerInput;
+    private int m_playerNum;
+
+    private GameManager m_gameManager;
+    private PlayerController m_playerController;
+
+    private void Awake()
     {
-        
+        m_playerInput = GetComponent<PlayerInput>();
+        m_playerController = GetComponent<PlayerController>();
+
+        m_gameManager = FindFirstObjectByType<GameManager>();
+
+        m_playerNum = m_playerInput.playerIndex;
+
+        Debug.Log(m_playerInput.GetDevice<InputDevice>());
+
+        Debug.Log(m_playerNum);
+
+        EnableOrDisablePlayerScript();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        EnableOrDisablePlayerScript();
+    }
+
+    private void EnableOrDisablePlayerScript()
+    {
+        if (m_gameManager.currentScene == "Noah") // Should be the lobby scene but just make this any test scene you want
+        {
+            m_playerController.enabled = false;
+        }
+        else
+        {
+            m_playerController.enabled = true;
+        }
     }
 }
