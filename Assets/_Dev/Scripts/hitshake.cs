7b2b0bec6792fc2acc7eecf0524c40278a1f42ef
Mathijnwis <144341993+Mathijnwis@users.noreply.github.com>
2025-02-01 14:25:52 +0100

    audio souce play

--- Assets/_Dev/Scripts/hitshake.cs
+++ Assets/_Dev/Scripts/hitshake.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using EasyTextEffects;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class hitshake : MonoBehaviour
 {
     public TextEffect textEffect;
+    public AudioSource audioSource;
     public void shake()
     {
         textEffect.StartManualEffect("shake");
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        } else
+        {
+            Debug.LogWarning("GEEN AUDO SOURCE PLS FIX RN");
+        }
         StartCoroutine(stopShake());
     }
     public IEnumerator stopShake()
